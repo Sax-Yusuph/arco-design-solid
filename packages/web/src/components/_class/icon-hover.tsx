aaf55e7d4c145452fe5c29e320512a831b9b66b8
sax-yusuph <yusuphshamsondeen@gmail.com>
2024-01-07 10:58:52 +0100

    fix hover overriding default class

---
+++ packages/web/src/components/_class/icon-hover.tsx
@@ -0,0 +1,33 @@
+import { JSX, ParentProps, splitProps } from 'solid-js'
+import cs from '../../utils/classNames'
+import { useConfigContext } from '../config-provider'
+
+interface HoverProps extends JSX.HTMLAttributes<HTMLSpanElement> {
+  size?: 'small' | 'mini' | 'default' | 'large'
+  prefix?: string
+  disabled?: boolean
+  onClick?: (e: MouseEvent) => void
+}
+
+export default function IconHover(props: ParentProps<HoverProps>) {
+  const [local, rest] = splitProps(props, ['class', 'disabled', 'prefix', 'size'])
+  const ctx = useConfigContext()
+  const prefixCls = ctx.getPrefixCls?.('icon-hover')
+
+  const size = local.size || 'default'
+
+  return (
+    <span
+      {...rest}
+      class={cs(
+        prefixCls,
+        {
+          [`${local.prefix}-icon-hover`]: local.prefix,
+          [`${prefixCls}-size-${local.size}`]: size && size !== 'default',
+          [`${prefixCls}-disabled`]: local.disabled,
+        },
+        props.class,
+      )}
+    />
+  )
+}

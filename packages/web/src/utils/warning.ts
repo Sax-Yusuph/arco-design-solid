677fdc04c4d6a091a267fc9b9169ba71d121d649
sax-yusuph <yusuphshamsondeen@gmail.com>
2024-01-08 10:18:16 +0100

    update logging

--- packages/web/src/utils/warning.ts
+++ packages/web/src/utils/warning.ts
@@ -2,7 +2,7 @@ export default function warning(condition: any, message: string) {
   if (process.env['NODE_ENV'] !== 'production') {
     if (condition) {
       // eslint-disable-next-line no-console
-      console.error(`[@arco-design/web-react]: ${message}`)
+      console.error(`[@arco-design/web-solid]: ${message}`)
     }
   }
 }

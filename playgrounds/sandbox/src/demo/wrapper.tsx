9971952eeb297f804690076cea24b780afb13e59
sax-yusuph <yusuphshamsondeen@gmail.com>
2024-01-08 10:19:17 +0100

    Update Demo wrapper ui

--- playgrounds/sandbox/src/demo/wrapper.tsx
+++ playgrounds/sandbox/src/demo/wrapper.tsx
@@ -1,12 +1,18 @@
-import { Card } from '@arco-design/web-solid';
+import { Card, Space } from '@arco-design/web-solid';
 import { ParentProps, children } from 'solid-js';
 
 export const Demo = (props: ParentProps<{ name: string; description?: string }>) => {
-  const child = children(() => props.children)
+  const description = children(() => props.description)
+
   return (
-    <Card title={props.name}>
-      <p>{props.description}</p>
-      {child()}
-    </Card>
+    <Space direction="vertical" style="width:100% !important">
+      <h6 class="arco-typography">{props.name}</h6>
+      {typeof description() === 'string' ? (
+        <p class="arco-typography">{props.description}</p>
+      ) : (
+        description
+      )}
+      <Card>{props.children}</Card>
+    </Space>
   )
 }
